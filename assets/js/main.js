c76392e12d46c489e1db0ed6b1e13870e9a6681a
fusiontecsoftware <johndoe@example.comsupport@fusiontec.com>
2025-05-26 12:44:10 +0530

    files added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Sailor
-* Template URL: https://bootstrapmade.com/sailor-free-bootstrap-theme/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
@@ -197,3 +189,4 @@
   window.addEventListener("load", initSwiper);
 
 })();
+
